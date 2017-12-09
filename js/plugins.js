8d924a973158efd1b84f97b27148a3bc9f661a1b
EmadEddin <aimad.tchad@gmail.com>
2017-12-09 22:55:38 +0000

    At this point all errors are done project is not responsive but it's done as a simple framwork for single page website, nice home with smooth deplacement between principal divs as services ...

--- js/plugins.js
+++ js/plugins.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 	var myHeader = $('.header');
 	var mySlider = $('.bxslider');
 	//set header automaticly to window height
-	myHeader.css('height',$(document).height());
+	myHeader.css('height',$(window).height());
 	
 
 	//fuction for the click menu element and change active classe among theme
@@ -25,17 +25,45 @@ $(document).ready(function() {
   	 mySlider.each(function(){
     	$(this).css('paddingTop',($(window).height() - $('.bxslider li').height())/2);
   	 });
+ 
 
   	 //fuction execute at windows resize
 	$(window).on('resize',function(){
 
-			$('.myheader').css('height',$(document).height());
+			myHeader.css('height',$(window).height());
 
 			mySlider.each(function(){
 		    	$(this).css('paddingTop',($(window).height() - $('.bxslider li').height())/2);
 
 		  	});
 	});
+
+	//smooth scroll betwin principale divs
+	  //explication : if we click at the element who has .abt as class which in this case
+	 //is the link About this will scrollTop by the size between the class of the second div position
+	//and the top line of the body thats why offset fuction and animate fuction only to smooth it 1,2 second 
+	 //>> but this code will take me only to the services only 
+	// $('.abt').on('click',function(){
+	// 	$('html,body').animate({
+	// 		scrollTop : $('.services').offset().top
+	// 	},1200);
+	// });
+
+	//to fix the previouse probleme we will work with data-libghiti='value'
+	$('.links li a').click(function(){
+		var a = $(this).data('value');
+		$('html, body').animate({
+			scrollTop : $('#' + $(this).data('value')).offset().top
+		}, 1000);
+
+	});	
+
+
+
+
+
+
+
 
 
 });
